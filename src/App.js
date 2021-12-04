f01448af189bd37fc0a243c6d14b6ac6b2bad49b
nguyenthanhduc <nguyenthanhduc4444@gmail.com>
2021-12-04 22:00:54 +0700

    a1

--- src/App.js
+++ src/App.js
@@ -172,7 +172,7 @@ function App() {
                 <div className="info">
                   <p className="semi-bold">University of Information Technology</p>
                   <p>
-                    Major : Information Systemaaaaaasdfasdfadsf
+                    Major : Information Systemaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
                   </p>
                 </div>
               </li>
