3c9971884f18962f5ba773fbcf35285277aaaf3c
nguyenthanhduc <nguyenthanhduc4444@gmail.com>
2021-12-04 22:01:14 +0700

    test1
    # Conflicts:
    #	src/App.js

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
                 <li>
                   <div className="icon"><i class="fas fa-map-signs"></i></div>
                   <div className="data">
-                    103 Lê Quyên, Phường 5, Quận 8 <br/> TPHCM
+                    103 Lê Quyên, Phường 5, Quận 8 <br/> TPHCMaaaaaa
                   </div>
                 </li>
                 <li>
@@ -172,7 +172,7 @@ function App() {
                 <div className="info">
                   <p className="semi-bold">University of Information Technology</p>
                   <p>
-                    Major : Information System master
+                    Major : Information Systemtesttttttttttt
                   </p>
                 </div>
               </li>
